efc6aa585a864ccc21ad2e021e9aae0498d47ddd
ArtemyBobkov <bobkov.aa@phystech.edu>
2021-03-19 22:28:50 +0300

    new branch added

---
+++ Game/coordinates.py
@@ -0,0 +1,9 @@
+import Game
+
+class Coordinates:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        print('Point set at coordinates: x = {0}; y = {1};'.format(*[x, y]))
+
