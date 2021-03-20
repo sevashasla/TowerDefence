41656fca847fe784888ea598209e70f6cec4d9ba
sevashasla <skorokhodov.vs@phystech.edu>
2021-03-20 19:13:50 +0300

    change

--- Display/Display_graphics.py
+++ Display/Display_graphics.py
@@ -1,21 +1,30 @@
 import pygame
 from Display import Display
 
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
 class DisplayGraphics(Display):
 	def __init__(self):
 		super().__init__()
-		self.size = (512, 512)
+		self.width = 512
+		self.height = 512
 	
 	def start(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode(self.size)
-
-	def run(self):
-		running = True
-		while running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					running = False
+		self.screen = pygame.display.set_mode((self.width, self.height))
 
 	def show(self, items):
-		pass
+		self.screen.fill(WHITE)
+		for item in items:
+			item.draw()
+		pygame.display.flip()
+
+	def finish(self):
+		pygame.quit()
+
